1e79271ca9fd36b258307b246557ca0318a64fc0
Teeraphat Phuwiang <bookmacbook@Book-MacBook-Air.local>
2026-01-03 03:26:41 +0700

    second upload

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,18 @@
 #ifndef get_next_line
 # define get_next_line
 
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *str);
+int ft_findNL(char *str);
+int findAD_NL(char *str);
+char *ft_strjoin(char *str1, char *str2);
+char *extract_NL(char *stash);
+char    *GNL(int fd);
+char *Read_stash(int fd, char *stash);
+char *clear_stash(char *stash);
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
