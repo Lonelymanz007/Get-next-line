d5c8fd06bb8f17423d0d88bd0398f526f8285d7a
Teeraphat Phuwiang <tphuwian@z2t14c4.42bangkok.com>
2026-01-07 16:33:36 +0700

    pass_addheader

--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tphuwian <tphuwian@student.42bangkok.com>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-01-05 15:27:05 by tphuwian          #+#    #+#             */
+/*   Updated: 2026-01-05 15:27:05 by tphuwian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H 
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
-
-size_t	ft_strlen(const char *str);
-int findAD_NL(char *str);
-char *ft_strjoin(char *str1, char *str2);
-char *extract_NL(char *stash);
-char	*get_next_line(int fd);
-char *Read_stash(int fd, char *stash);
-char *clear_stash(char *stash);
-char *ft_strdup( char *s1);
-char *for_free(char *stash);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
 
-# endif
+size_t	ft_strlen(const char *str);
+int		find_ad_nl(char *str);
+char	*ft_strjoin(char *str1, char *str2);
+char	*extract_nl(char *stash);
+char	*get_next_line(int fd);
+char	*read_stash(int fd, char *stash);
+char	*clear_stash(char *stash);
+char	*ft_strdup( char *s1);
+char	*for_free(char *stash, char *box);
+
+#endif
